678b7260ac13a493137e52cf5711c82f082b11bc
0num4 <oonuma.ryouyu+1@plus-zero.co.jp>
2021-09-25 15:28:35 +0900

    async/awaitを使うように

--- create-react-sample/src/components/helloworldcomponents.tsx
+++ create-react-sample/src/components/helloworldcomponents.tsx
@@ -1,16 +1,17 @@
 // apiに通信してhelloworldを返すapi componets
 import axios from "axios";
 import React, {useState, useEffect} from 'react'
-import { hello } from "../api/helloworld"
+// import { hello } from "../api/helloworld"
 
 type AppProps = { message: string }; // interfaceでもよい
 const Helloworld =({ message }: AppProps) => {
     const [posts, setPosts] = useState([])
     useEffect(() => {
-        axios.get('https://9s9unqlpkk.execute-api.ap-northeast-1.amazonaws.com/dev')
-        .then(res => {
+        const getHelloData = async ()=>{
+            const res = await axios.get('https://9s9unqlpkk.execute-api.ap-northeast-1.amazonaws.com/dev')
             setPosts(res.data)
-        })
+        }
+        getHelloData()
     }, [])
     return <div>
        AAA {posts} AAA      
