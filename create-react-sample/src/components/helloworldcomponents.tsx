615a1aff3bd5fb20906f7962481fb6595b6f20a5
0num4 <oonuma.ryouyu+1@plus-zero.co.jp>
2021-09-26 16:31:47 +0900

    api/以下のモジュールを使って動くように

--- create-react-sample/src/components/helloworldcomponents.tsx
+++ create-react-sample/src/components/helloworldcomponents.tsx
@@ -1,14 +1,14 @@
 // apiに通信してhelloworldを返すapi componets
-import axios from "axios";
 import React, {useState, useEffect} from 'react'
-// import { hello } from "../api/helloworld"
+import { hello } from "../api/helloworld"
 
 type AppProps = { message: string }; // interfaceでもよい
 const Helloworld =({ message }: AppProps) => {
     const [posts, setPosts] = useState([])
     useEffect(() => {
         const getHelloData = async ()=>{
-            const res = await axios.get('https://9s9unqlpkk.execute-api.ap-northeast-1.amazonaws.com/dev')
+            // const res = await axios.get('https://9s9unqlpkk.execute-api.ap-northeast-1.amazonaws.com/dev')
+            const res = await hello()
             setPosts(res.data)
         }
         getHelloData()
